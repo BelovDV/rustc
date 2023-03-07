8547c3e2eb3c5b42758710d28cdaaa68bf971f3e
BelovDV <70999565+BelovDV@users.noreply.github.com>
2023-03-07 17:55:57 +0300

    [temp] do not silently ignore error with getting jobserver

--- compiler/rustc_data_structures/src/jobserver.rs
+++ compiler/rustc_data_structures/src/jobserver.rs
@@ -1,4 +1,6 @@
 pub use jobserver_crate::Client;
+pub use jobserver_crate::ErrFromEnv;
+
 use std::sync::LazyLock;
 
 // We can only call `from_env` once per process
@@ -18,23 +20,25 @@ use std::sync::LazyLock;
 // Also note that we stick this in a global because there could be
 // multiple rustc instances in this process, and the jobserver is
 // per-process.
-static GLOBAL_CLIENT: LazyLock<Client> = LazyLock::new(|| unsafe {
-    Client::from_env().unwrap_or_else(|| {
-        let client = Client::new(32).expect("failed to create jobserver");
-        // Acquire a token for the main thread which we can release later
-        client.acquire_raw().ok();
-        client
-    })
+static GLOBAL_CLIENT: LazyLock<(Client, Option<ErrFromEnv>)> = LazyLock::new(|| unsafe {
+    match Client::from_env_ext() {
+        Ok(Some(c)) => (c, None),
+        Ok(None) => (Client::new(32).expect("failed to create jobserver"), None),
+        Err(e) => (Client::new(1).unwrap(), Some(e)),
+    }
 });
 
-pub fn client() -> Client {
-    GLOBAL_CLIENT.clone()
+pub fn client() -> Result<Client, ErrFromEnv> {
+    match GLOBAL_CLIENT.clone().1 {
+        Some(e) => Err(e),
+        None => Ok(GLOBAL_CLIENT.0.clone()),
+    }
 }
 
 pub fn acquire_thread() {
-    GLOBAL_CLIENT.acquire_raw().ok();
+    GLOBAL_CLIENT.0.acquire_raw().ok();
 }
 
 pub fn release_thread() {
-    GLOBAL_CLIENT.release_raw().ok();
+    GLOBAL_CLIENT.0.release_raw().ok();
 }
